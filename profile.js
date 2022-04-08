5c0b808c0a34364cc24559e66f6c264e37fbef92
Andrea Walsh <andreawalsh@CartersM1MacBook.local>
2022-04-08 15:15:16 -0400

    Second commit

--- profile.js
+++ profile.js
@@ -1,7 +1,17 @@
-let color = document.getElementById("#color")
+let color = document.querySelector("#color")
+let place = document.querySelector("#place")
+let ritual = document.querySelector("#ritual")
 
-const button = () => {
-    if()
+const colorAlert = () => {
+    alert("My favorite color is red!")
+}
+const placeAlert = () => {
+    alert("My favorite place is on a mountain.")
+}
+const ritualAlert = () => {
+    alert("My favorite ritual is eating")
 }
 
-btn.addEventListener("click", button)
+color.addEventListener("click", colorAlert)
+place.addEventListener("click", placeAlert)
+ritual.addEventListener("click", ritualAlert)
